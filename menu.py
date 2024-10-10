bca05eae5dc326be0fbc057f0c91a1278f26ad85
Damian Pazos <damian_ariel_pazos>
2024-10-10 01:15:42 -0300

    menu and options selection

---
+++ menu.py
@@ -0,0 +1,32 @@
+# Menu de PFI
+
+def imprimir_menu ():
+    print('MENU DE OPCIONES\n\nSeleccione el numero de la opcion solicitada')
+    print('1. Registrar producto\n2. Modificar producto\n3. Eliminar producto\n4. Listado de productos\n5. Productos con poco stock\n6. Busqueda de producto\n7. Salir del programa\n\n')
+    numero_opcion = int(input())
+    return numero_opcion
+
+# Seleccion de opcion
+def seleccion_opcion(numero) :
+    if numero == 1 :
+        #TODO: funcion registrar
+        pass
+    elif numero == 2 :
+        pass
+        #TODO: funcion modificar
+    elif numero == 3 :
+        pass
+        #TODO: funcion eliminar
+    elif numero == 4:
+        pass
+        #TODO: funcion listar
+    elif numero == 5:
+        #TODO: funcion poco stock
+        pass
+    elif numero == 6:
+        #TODO: funcion buscar
+        pass
+    elif numero == 7:
+        #TODO: funcion salir
+        pass
+
