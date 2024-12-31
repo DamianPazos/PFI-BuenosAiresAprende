83e7fe182776f24a3699b4ecb08b91c794bb30b3
Damian Pazos <damian_ariel_pazos>
2024-12-30 22:42:45 -0300

    database

--- functions.py
+++ functions.py
@@ -1,76 +1,132 @@
+import sqlite3
+
 # Registro de producto
 def registro(almacenamiento):
     try:        
         nombre = input('Ingrese el nombre del producto: ')
+        descripcion = input('Ingrese la descripción del producto: ')
         precio = float(input('Ingrese el precio del producto: $'))
         stock = int(input('Ingrese la cantidad de stock del producto: '))
-
-        producto_final = {
-            'nombre': nombre,
-            'precio' : precio,
-            'stock': stock,
-        }
-        almacenamiento.append(producto_final)
-    except:
+        categoria = input('Ingrese la categoria del producto: ')
+        almacenamiento.execute('''
+            INSERT INTO productos (nombre, descripcion, cantidad, precio, categoria)
+            VALUES (?, ?, ?, ?, ?)
+            ''', (nombre, descripcion, stock, precio, categoria))
+        almacenamiento.commit()
+    except ValueError:
         print('Ingresaste un valor no valido, reintentalo nuevamente')
-        registro(almacenamiento)    
+        registro(almacenamiento)
+    except sqlite3.Error as e:
+        print("Hubo un error en el registro del producto: {e}")  
 
 # Modificacion de un producto
 # TODO: Opcion salir
 # TODO: Expeciones
-'''
-def modificar(producto, almacenamiento):
-    for producto_modificar in almacenamiento:
-        if producto_modificar == producto:
-            opcion_clave = int(input('Que desea modificar:\n1. Nombre\n2. Precio\n3. Stock\n'))
-            if opcion_clave == 1:
-                clave = 'nombre'
-            elif opcion_clave == 2:
-                clave = 'precio'
-            elif opcion_clave == 3:
-                clave = 'stock'
-            new_valor = input('Ingrese el nuevo valor')
-            producto_modificar[clave] = new_valor
-'''
 
-# Busqueda de producto
-def buscar(almacenamiento):
+def modificar_cantidad(cursor, almacenamiento):
+    try:
+        producto_modificado = int(input('¿Que producto desea modificar?\n'))
+        valor = int(input('¿Cual es la nueva cantidad?\n'))
+        cursor.execute('''
+                        UPDATE productos SET cantidad = ?
+                        WHERE id = ?
+                        ''',(valor, producto_modificado,))
+        almacenamiento.commit()
+    except ValueError:
+        print('Ingreso un valor invalido, reintentelo nuevamente')
+        modificar_cantidad(cursor, almacenamiento)
+    except sqlite3.Error as e:
+        print(f"Hubo un error en la modificacion del producto: {e}")
+
+# Busqueda de producto (ID)
+def buscar(cursor):
     try:
         valor = input('¿Que producto desea buscar?\n')
-        for producto in almacenamiento:
-            if producto['nombre'] == valor:
-                return producto
-        return None
-    except:
+        cursor.execute('''
+                        SELECT * FROM productos
+                        WHERE id = ?
+                        ''', (valor,))
+        return cursor.fetchall()
+    except ValueError:
         print('Ingreso un valor invalido, reintentelo nuevamente')
-        buscar(almacenamiento)
+        buscar(cursor)
+    except sqlite3.Error as e:
+        print(f"Hubo un error en la busqueda del producto: {e}")
 
 # Eliminar producto
-def eliminar(producto, almacenamiento):
-    almacenamiento.remove(producto)
+def eliminar(cursor, almacenamiento):
+    try:
+        valor = int(input('¿Que producto desea eliminar?\n'))
+        cursor.execute('''
+                        DELETE FROM productos
+                        WHERE id = ?
+                        ''', (valor,))
+        almacenamiento.commit()
+    except ValueError:
+        print('Ingreso un valor invalido, reintentelo nuevamente')
+        eliminar(cursor, almacenamiento)
+    except sqlite3.Error as e:
+        print(f"Hubo un error en la eliminacion del producto: {e}")
 
 # Enlista los productos
-def enlistar(almacenamiento):
-    contador = 1
-    for producto in almacenamiento:
-        print(f"Producto N°{contador}\nNombre: {producto['nombre']}\nPrecio: ${producto['precio']}\nStock: {producto['stock']}\n")
-        contador = contador + 1
+def enlistar(cursor):
+    try:
+        cursor.execute('''
+                        SELECT * FROM productos
+                        ''')
+        return cursor.fetchall()
+    except ValueError:
+        print('Ingreso un valor invalido, reintentelo nuevamente')
+        enlistar(cursor)
+    except sqlite3.Error as e:
+        print(f"Hubo un error en la muestra de los productos: {e}")
 
 
 # Muestra menu principal
 def mostrar_menu_principal ():
     print('\n\nMENU DE OPCIONES\n\n')
-    print('1. Registrar producto\n2. Mostrar productos\n3. Eliminar producto\n4. Salir del programa\n\n')
+    print('1. Registrar producto\n2. Buscar producto\n3. Eliminar producto\n4. Enlistar productos\n5. Modificar producto\n6. Reporte de bajo stock\n7. Salir del programa\n\n')
 
-'''
-def menu_modificar():
-    print('Que valor desea modificar\n1. Nombre\n2. Precio\n3. Stock\n4. Atras')
-    opcion = int(input())
-    if opcion == 1:
-        return 'Nombre'
-    elif opcion == 2:
-        return 'Precio'
-    elif opcion == 3:
-        return 'Stock'
-    else:
-        pass'''
+# Conexion y/o creacion de base de datos
+def conectar_base(nombre):       
+    try:
+        conexion = sqlite3.connect(nombre)
+        return conexion
+    except sqlite3.Error as e:
+        print(f"Error con la conexion en la base de datos: {e}")
+        return None
+    
+# Creacion de cursor para base de datos
+def crear_cursor(conexion):
+    if conexion:
+        try:
+            return conexion.cursor()
+        except sqlite3.Error as e:
+            print(f'Error al crear cursor para base de datos: {e}')
+            return None
+        
+# Guardar cambios en base de datos
+def guardar_cambios(conexion):
+    if conexion:
+        try:
+            conexion.commit()
+        except sqlite3.Error as e:
+            print(f"Error al querer guardar los cambios en la base de datos: {e}")
+
+# Existencia de tabla
+def existe_tabla(nombre, cursor):
+    try:
+        cursor.execute(f"SELECT * FROM {nombre} LIMIT 1")
+        return True
+    except sqlite3.OperationalError:
+        return False
+    
+# Reporte de bajo stock
+def reportar_bajo_stock(cursor):
+    try:
+        cursor.execute('''
+                       SELECT * FROM productos WHERE cantidad < 20
+                       ''',)
+        return cursor.fetchall()
+    except sqlite3.Error as e:
+        print(f"Hubo un error en la generacion del reporte: {e}")
--- main.py
+++ main.py
@@ -1,23 +1,43 @@
 import functions as func
 
-productos = []
+# Base de datos
+base_datos = "inventario.db"
+tabla = "productos"
+conexion_bd = func.conectar_base(base_datos)
+cursor_bd = func.crear_cursor(conexion_bd)
+if not func.existe_tabla(tabla, conexion_bd):
+    conexion_bd.execute('''
+            CREATE TABLE IF NOT EXISTS ? (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT NOT NULL,
+            descripcion TEXT,
+            cantidad INTEGER NOT NULL,
+            precio REAL NOT NULL,
+            categoria TEXT
+            )
+            ''', (tabla))
+    func.guardar_cambios(conexion_bd)
 
 while True:
+    # Menu principal
     func.mostrar_menu_principal()
+    
     try:
         opcion_menu_principal = int(input('Ingrese el número de opción: '))
         if opcion_menu_principal == 1:
-            func.registro(productos)
+            func.registro(conexion_bd)
         elif opcion_menu_principal == 2:
-            func.enlistar(productos)
+            print(func.buscar(cursor_bd))
         elif opcion_menu_principal == 3:
-            busqueda = func.buscar(productos)
-            if busqueda != None:
-                func.eliminar(busqueda, productos)
-                print('Se elimino correctamente')
-            else:
-                print('No se encontro producto')
+            func.eliminar(cursor_bd, conexion_bd)
         elif opcion_menu_principal == 4:
+            print(func.enlistar(cursor_bd))
+        elif opcion_menu_principal == 5:
+            func.modificar_cantidad(cursor_bd, conexion_bd)
+        elif opcion_menu_principal == 6:
+            print(func.reportar_bajo_stock(cursor_bd))    
+        elif opcion_menu_principal == 7:
+            conexion_bd.close()
             print('Usted ha elegido cerrar el programa')
             break
         else: 
